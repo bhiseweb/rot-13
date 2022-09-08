941c5dc9001c976c40c852c303964b0932d8d776
Leena <leena.web11@gmail.com>
2022-09-08 14:48:50 +0530

    ROT-13 converter added

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,15 +1,15 @@
 class ConversionsController < ApplicationController
 
   def new
-    @conversion = Conversion.new
+    conversion = Conversion.new
   end
 
   def create
-    @conversion = Conversion.new(conversion_params)
-    if @conversion.save
-      render json: {message: "data has been created"}
+    conversion = Conversion.new(conversion_params)
+    if conversion.save
+      render json: {message: "Data has been created and Rot-13 version is #{conversion.rot}"}
     else
-      render json: {message: @conversion.errors.messages}
+      render json: {message: conversion.errors.messages}
     end
   end
 
--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,3 +1,14 @@
 class Conversion < ApplicationRecord
   validates :body, presence: true, length: { maximum: 1000 }
+
+  def rot
+    string = self.body
+    string.gsub(/./) do |char|
+      case char
+      when 'a'..'m', 'A'..'M' then (char.ord + 13).chr
+      when 'n'..'z', 'N'..'Z' then (char.ord - 13).chr
+      else char
+      end
+    end
+  end
 end
