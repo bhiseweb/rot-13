74bef964446c526f0523f48490733898020c9a3d
Leena <leena.web11@gmail.com>
2022-09-08 15:40:02 +0530

    Hotfixes of rubocop

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,5 +1,7 @@
-class ConversionsController < ApplicationController
+# frozen_string_literal: true
 
+# app/controllers/conversions_controller.rb
+class ConversionsController < ApplicationController
   def new
     conversion = Conversion.new
   end
@@ -7,14 +9,15 @@ class ConversionsController < ApplicationController
   def create
     conversion = Conversion.new(conversion_params)
     if conversion.save
-      render json: {message: "Data has been created and Rot-13 version is #{conversion.rot}"}
+      render json: { message: "Data has been created and Rot-13 version is #{conversion.rot}" }
     else
-      render json: {message: conversion.errors.messages}
+      render json: { message: conversion.errors.messages }
     end
   end
 
   private
-    def conversion_params
-      params.require(:conversion).permit(:body)
-    end
+
+  def conversion_params
+    params.require(:conversion).permit(:body)
+  end
 end
--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
+# app/models/conversion.rb
 class Conversion < ApplicationRecord
   validates :body, presence: true, length: { maximum: 1000 }
 
   def rot
-    string = self.body
+    string = body
     string.gsub(/./) do |char|
       case char
       when 'a'..'m', 'A'..'M' then (char.ord + 13).chr
